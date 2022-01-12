64ec93b4fe2e4fc8161f2809c0724101533fa789
Yazdun <yazduntube@gmail.com>
2022-01-12 13:49:07 +0330

    general : edited homepage

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,12 +1,13 @@
 import s from "./styles.module.scss";
 import classnames from "classnames";
+import { Container } from "../../ui";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   return (
-    <>
-      <div>
-        <h1>Homepage</h1>
-      </div>
-    </>
+    <Container>
+      <h1>Homepage</h1>
+      <Link to="/login">login</Link>
+    </Container>
   );
 };
