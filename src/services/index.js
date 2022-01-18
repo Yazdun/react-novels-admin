667ff806041e09f771f2e4d13cfe2b9950fcd60b
Yazdun <yazduntube@gmail.com>
2022-01-18 11:16:42 +0330

    services : edited a route

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,3 @@
 export const GET_ALL_AUTHORS = "/admin/author/";
-export const GET_SINGLE_AUTHOR = (id) => {
-  return `/admin/author/find/${id}`;
-};
+export const GET_SINGLE_AUTHOR = (id) => `/admin/author/find/${id}`;
 export const CREATE_AUTHOR = "/admin/author/create";
